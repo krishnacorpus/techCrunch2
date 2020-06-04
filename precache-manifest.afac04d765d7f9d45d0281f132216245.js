ca2d6ac0a1626645d311e6995155867f8bd59461
krishnacorpus <krishna.kondapalli@imversion.com>
2020-06-04 11:03:23 +0530

    Updates

---
+++ precache-manifest.afac04d765d7f9d45d0281f132216245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a9d5fe68f9fe9bc59b49f7bce43ae7",
+    "url": "/techCrunch2/index.html"
+  },
+  {
+    "revision": "19ff2dc78cad126f5f4b",
+    "url": "/techCrunch2/static/css/main.06d13be9.chunk.css"
+  },
+  {
+    "revision": "6e1cc774f35539ca485d",
+    "url": "/techCrunch2/static/js/2.22c7a93f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/techCrunch2/static/js/2.22c7a93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ff2dc78cad126f5f4b",
+    "url": "/techCrunch2/static/js/main.3594eab9.chunk.js"
+  },
+  {
+    "revision": "85c02c1f6d1665146007",
+    "url": "/techCrunch2/static/js/runtime-main.9fd7dc8d.js"
+  }
+]);
